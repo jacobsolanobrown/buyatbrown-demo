c40b057d44d52a55f6e93822e9a9e62fb97bb9f9
“bisolaf” <adedoyin-folarin@brown.edu>
2024-11-30 14:09:19 -0500

    setup

---
+++ src/main/java/edu/brown/cs/student/main/Storage/FirebaseUtilities.java
@@ -0,0 +1,20 @@
+import com.google.api.core.ApiFuture;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+public class FirebaseUtilities {
+
+}
---
+++ src/main/java/edu/brown/cs/student/main/Storage/StorageInterface.java
@@ -0,0 +1,4 @@
+package edu.brown.cs.student.main.Storage;
+
+public class StorageInterface {
+}
