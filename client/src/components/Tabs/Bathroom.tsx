57c238837b3c6be6fe80d4949f35ddfe59283940
kayleencruz <kayleen_cruz@brown.edu>
2024-12-14 14:50:31 -0500

    fixed images in subpages

--- client/src/components/Tabs/Bathroom.tsx
+++ client/src/components/Tabs/Bathroom.tsx
@@ -89,7 +89,7 @@ export default function Bathroom () {
             posts.map((post) => ( // posts should reflect 
             <ListingCard
               key={post.id}
-              imageUrl={post.id} 
+              imageUrl={post.imageUrl} 
               title={post.title}
               price={post.price}
               username={post.username}
