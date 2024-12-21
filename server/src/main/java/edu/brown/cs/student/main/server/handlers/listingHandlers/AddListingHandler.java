956ac62f8cb5e1a51cdc9a8e61c52a4b3179a206
cscicla <claire_y_zhang@brown.edu>
2024-12-21 06:20:14 -0600

    test: added tags testing

--- server/src/main/java/edu/brown/cs/student/main/server/handlers/listingHandlers/AddListingHandler.java
+++ server/src/main/java/edu/brown/cs/student/main/server/handlers/listingHandlers/AddListingHandler.java
@@ -7,10 +7,12 @@ import edu.brown.cs.student.main.server.handlers.Utils;
 import edu.brown.cs.student.main.server.storage.GoogleCloudStorageInterface;
 import edu.brown.cs.student.main.server.storage.GoogleCloudStorageUtilities;
 import edu.brown.cs.student.main.server.storage.StorageInterface;
+import java.lang.reflect.Array;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -18,6 +20,7 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import javax.sound.midi.SysexMessage;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -42,21 +45,20 @@ public class AddListingHandler implements Route {
    * @param text A String input of word(s) separated by commas
    * @return An int representing the number of words between commas
    */
-  public static int countWordsBetweenCommas(String text) {
-    int count = 1; // assume that there is at least one word
-    boolean insideComma = false;
+  public static Integer countWordsBetweenCommas(String text) {
+    // Split the string by commas
+    String[] parts = text.split(",");
+    ArrayList<Integer> totalWords = new ArrayList<>();
+    int partWords = 0;
 
-    for (int i = 0; i < text.length(); i++) {
-      char ch = text.charAt(i);
+    for (String part : parts) {
 
-      if (ch == ',') {
-        insideComma = !insideComma;
-      } else if (Character.isWhitespace(ch) && insideComma) { // if reach space
-        count++;
-      }
+      String[] words = part.trim().split("\\s+");
+      int len_words = words.length;
+      totalWords.add(len_words);
     }
-
-    return count;
+    // Return the maximum count from the list
+    return Collections.max(totalWords);
   }
 
   /**
@@ -68,10 +70,7 @@ public class AddListingHandler implements Route {
    */
   public static boolean noDuplicateEntries(String text) {
     HashSet<String> noDuplicateEntries = new HashSet<String>(Arrays.asList(text.split(",")));
-    System.out.println(noDuplicateEntries.size());
     ArrayList<String> duplicateEntries = new ArrayList<String>(Arrays.asList(text.split(",")));
-    System.out.println(duplicateEntries.size());
-    System.out.println(noDuplicateEntries.size() == duplicateEntries.size());
     return (noDuplicateEntries.size() == duplicateEntries.size());
   }
 
@@ -88,6 +87,8 @@ public class AddListingHandler implements Route {
       throw new IllegalArgumentException("Please specify user. Input both the UID and Username.");
     }
 
+    uid = uid.trim();
+    username = username.trim();
     List<Map<String, Object>> userDatamaps = storageHandler.getAllUserDataMaps();
     for (Map<String, Object> datamap : userDatamaps) {
       if (datamap.get("uid") == uid) {
@@ -131,12 +132,17 @@ public class AddListingHandler implements Route {
         "All listings arguments are required "
           + "(title, tags, price, image, category, condition, description)");
     }
+
+    tags = tags.trim();
+    // Remove whitespace greater than 1 between words and before/after commas
+    tags = tags.replaceAll("\\s{2,}", " ");
+
     // there should be no extra spaces and  tags are in the form "tag1,tag2,tag3, two wordtag"
     if (tags.length() - tags.replace("  ", "").replace(" ,", ",").replace(", ", ",").length() > 0) {
       System.out.println(
-          "Each tag should only have ONE space between words and non before and after commas.");
+          "Each tag should only have NO space between words and non before and after commas.");
       throw new IllegalArgumentException(
-          "Each tag should only have ONE space between words and non before and after commas.");
+          "Each tag should only have NO space between words and non before and after commas.");
     }
 
     // each tag should be less than or equal to 2 words
@@ -146,12 +152,13 @@ public class AddListingHandler implements Route {
     }
 
     // tags are in the form "tag1,tag2,tag3, two wordtag"
-    if (tags.length() - tags.replace(",,", "").replace(", ,", "").length() > 0) {
+    if (tags.length() - tags.replace(",  ,", "").replace(",,", "").replace(", ,", "").length() > 0) {
       System.out.println("Each tag should have a value.");
       throw new IllegalArgumentException("Each tag should have a value.");
     }
 
     // if there are more than 5 tags, error
+
     if (tags.length() - tags.replace(",", "").length() > 4) {
       System.out.println("Please input less than or equal to 5 tags.");
       throw new IllegalArgumentException("Please input less than or equal to 5 tags.");
@@ -159,8 +166,8 @@ public class AddListingHandler implements Route {
 
     // if there are repeated tags, error
     if (!noDuplicateEntries(tags)) {
-      System.out.println("Please make sure all tags are unique");
-      throw new IllegalArgumentException("Please make sure all tags are unique");
+      System.out.println("Please make sure all tags are unique.");
+      throw new IllegalArgumentException("Please make sure all tags are unique.");
     }
   }
 
@@ -215,6 +222,8 @@ public class AddListingHandler implements Route {
         "All listings arguments are required "
           + "(title, tags, price, image, category, condition, description)");
     }
+
+    title = title.trim();
     // check if title is less than 40 characters
     if (title.length() > 40) {
       System.out.println("Title must be less than or equal to 40 characters.");
@@ -237,7 +246,7 @@ public class AddListingHandler implements Route {
     }
 
     // check if condition option is one of the valid options
-    condition = condition.toLowerCase();
+    condition = condition.trim().toLowerCase();
     if (!(condition.equals("new") || condition.equals("like new") || condition.equals("used"))) {
       System.out.println(
         "Please choose from valid condition inputs (i.e. new, like new, or Used).");
@@ -260,6 +269,7 @@ public class AddListingHandler implements Route {
           + "(title, tags, price, image, category, condition, description)");
     }
 
+
     if (category.contains(",")) {
       System.out.println("Category can only have one value (i.e.value must not contain commas).");
       throw new IllegalArgumentException(
@@ -267,7 +277,7 @@ public class AddListingHandler implements Route {
     }
 
     // check if category option is one of the valid options
-    category = category.toLowerCase();
+    category = category.trim().toLowerCase();
     List<String> categoryList = new ArrayList<String>();
     // check if its in list
 //    categoryList.add("clothes").add("tech"), "school", "furniture", "kitchen", "bathroom");
--- server/src/test/java/edu/brown/cs/student/main/server/test/AddListingHandlerTest.java
+++ server/src/test/java/edu/brown/cs/student/main/server/test/AddListingHandlerTest.java
@@ -1,5 +1,6 @@
 package edu.brown.cs.student.main.server.test;
 
+import static edu.brown.cs.student.main.server.handlers.listingHandlers.AddListingHandler.countWordsBetweenCommas;
 import static org.junit.jupiter.api.Assertions.*;
 
 import edu.brown.cs.student.main.server.handlers.Utils;
@@ -15,6 +16,7 @@ import edu.brown.cs.student.main.server.storage.StorageInterface;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeAll;
@@ -30,8 +32,9 @@ class AddListingHandlerTest {
   private static AddListingHandler mockAddListingHandler;
   private static AddListingHandler addListingHandler;
 
-  @BeforeEach
-  public void setUp() throws IOException {
+
+  @BeforeAll
+  public static void setUp() throws IOException {
     Map<String, Object> listing = new HashMap<>();
 
     // Set up mocking
@@ -69,7 +72,8 @@ class AddListingHandlerTest {
         + " to go to worst. They are really nice. Let me know if you would like any!");
 
     String workingDirectory = System.getProperty("user.dir");
-    Path base64TestPath = Paths.get(workingDirectory, "src/test/java/edu/brown/cs/student/main/server/test/data/base64_testing_image.txt");
+    Path base64TestPath = Paths.get(workingDirectory,
+      "src/test/java/edu/brown/cs/student/main/server/test/data/base64_testing_image.txt");
     String body = new String(Files.readAllBytes(base64TestPath));
     MockRequest mockRequest = new MockRequest(queryParams, body);
 
@@ -91,11 +95,276 @@ class AddListingHandlerTest {
     assertEquals("10.99", responseMap.get("price"));
     assertEquals("New", responseMap.get("condition"));
     assertEquals("I bought some Muji black pens in high school and brought them to Brown. "
-      + "Since coming here though, I only take notes using my ipad. I would hate for these pens"
-      + " to go to worst. They are really nice. Let me know if you would like any!", responseMap.get("description"));
+        + "Since coming here though, I only take notes using my ipad. I would hate for these pens"
+        + " to go to worst. They are really nice. Let me know if you would like any!",
+      responseMap.get("description"));
     assertTrue(responseMap.get("imageUrl").toString().contains("http://mock-storage/"));
-
-    // Check if the listing is in the database
   }
 
-}
+  @Test
+  void testCountWordsBetweenCommas() {
+    assertEquals(2, countWordsBetweenCommas("more, than 2, words, here"));
+  }
+
+
+  @Test
+  void testInvalidTags() throws ExecutionException, InterruptedException, IOException {
+    Map<String, String> queryParams = new HashMap<>();
+
+    //################### Test for null tags ###################
+    queryParams.put("uid", "user123");
+    queryParams.put("username", "testuser");
+    queryParams.put("title", "Muji black pens");
+    queryParams.put("tags", null);
+    queryParams.put("category", "School");
+    queryParams.put("price", "10.99");
+    queryParams.put("condition", "New");
+    queryParams.put("description",
+      "I bought some Muji black pens in high school and brought them to Brown."
+        + " Since coming here though, I only take notes using my ipad. I would hate for these pens"
+        + " to go to worst. They are really nice. Let me know if you would like any!");
+
+    String workingDirectory = System.getProperty("user.dir");
+    Path base64TestPath = Paths.get(workingDirectory,
+      "src/test/java/edu/brown/cs/student/main/server/test/data/base64_testing_image.txt");
+    String body = new String(Files.readAllBytes(base64TestPath));
+    MockRequest nullTagMockRequest = new MockRequest(queryParams, body);
+
+    MockResponse mockResponse = new MockResponse();
+    Object nullTagResult = mockAddListingHandler.handle(nullTagMockRequest, mockResponse);
+    Map<String, Object> nullTagResponseMap = Utils.fromMoshiJson(nullTagResult.toString());
+    assertEquals("failure", nullTagResponseMap.get("response_type"));
+    assertEquals("All listings arguments are required (title, tags, price, "
+      + "image, category, condition, description)", nullTagResponseMap.get("error"));
+
+    //################### Check for Blank Tag ###################
+    queryParams.put("tags", "");
+    MockRequest emptyTagMockRequest = new MockRequest(queryParams, body);
+    Object emptyTagResult = mockAddListingHandler.handle(emptyTagMockRequest, mockResponse);
+    Map<String, Object> emptyTagResponseMap = Utils.fromMoshiJson(emptyTagResult.toString());
+    assertEquals("failure", emptyTagResponseMap.get("response_type"));
+    assertEquals("All listings arguments are required (title, tags, price, "
+      + "image, category, condition, description)", emptyTagResponseMap.get("error"));
+
+    //################### Check for Empty Tag ###################
+    queryParams.put("tags", "   ");
+    MockRequest blankTagMockRequest = new MockRequest(queryParams, body);
+    Object blankTagResult = mockAddListingHandler.handle(blankTagMockRequest, mockResponse);
+    Map<String, Object> blankTagResponseMap = Utils.fromMoshiJson(blankTagResult.toString());
+    assertEquals("failure", blankTagResponseMap.get("response_type"));
+    assertEquals("All listings arguments are required (title, tags, price, "
+      + "image, category, condition, description)", blankTagResponseMap.get("error"));
+
+    //################### Check for ill-formed Tag field ###################
+    // spaces between commas
+    queryParams.put("tags", "tag1, tag2");
+    MockRequest illFormatedTagMockRequest = new MockRequest(queryParams, body);
+    Object illFormatedTagResult = mockAddListingHandler.handle(illFormatedTagMockRequest,
+      mockResponse);
+    Map<String, Object> illFormatedResponseMap = Utils.fromMoshiJson(
+      illFormatedTagResult.toString());
+    assertEquals("failure", illFormatedResponseMap.get("response_type"));
+    assertEquals("Each tag should only have NO space between words and non before and "
+      + "after commas.", illFormatedResponseMap.get("error"));
+
+    // another potential invalid input
+    queryParams.put("tags", "tag1 ,tag2");
+    MockRequest illFormatedTag2MockRequest = new MockRequest(queryParams, body);
+    Object illFormatedTag2Result = mockAddListingHandler.handle(illFormatedTag2MockRequest,
+      mockResponse);
+    Map<String, Object> illFormated2ResponseMap = Utils.fromMoshiJson(
+      illFormatedTag2Result.toString());
+    assertEquals("failure", illFormatedResponseMap.get("response_type"));
+    assertEquals("Each tag should only have NO space between words and non before and "
+      + "after commas.", illFormatedResponseMap.get("error"));
+
+    // double commas
+    queryParams.put("tags", "tag1,,tag2");
+    MockRequest wrongTagNumberMockRequest = new MockRequest(queryParams, body);
+    Object wrongTagNumberResult = mockAddListingHandler.handle(wrongTagNumberMockRequest,
+      mockResponse);
+    Map<String, Object> wrongTagNumberResponseMap = Utils.fromMoshiJson(
+      wrongTagNumberResult.toString());
+    assertEquals("failure", wrongTagNumberResponseMap.get("response_type"));
+    assertEquals("Each tag should have a value.", wrongTagNumberResponseMap.get("error"));
+
+    // double commas
+    queryParams.put("tags", "tag1,   ,tag2");
+    MockRequest wrongTagNumber2MockRequest = new MockRequest(queryParams, body);
+    Object wrongTagNumber2Result = mockAddListingHandler.handle(wrongTagNumber2MockRequest,
+      mockResponse);
+    Map<String, Object> wrongTagNumber2ResponseMap = Utils.fromMoshiJson(
+      wrongTagNumber2Result.toString());
+    assertEquals("failure", wrongTagNumber2ResponseMap.get("response_type"));
+    assertEquals(
+      "Each tag should only have NO space between words and non before and after commas.",
+      wrongTagNumber2ResponseMap.get("error"));
+
+    // more than 5 tags
+    queryParams.put("tags", "tag1,tag2,tag3,tag4,tag5,tag6");
+    MockRequest tooManyTagsMockRequest = new MockRequest(queryParams, body);
+    Object tooManyTagsResult = mockAddListingHandler.handle(tooManyTagsMockRequest, mockResponse);
+    Map<String, Object> tooManyTagsResponseMap = Utils.fromMoshiJson(tooManyTagsResult.toString());
+    assertEquals("failure", tooManyTagsResponseMap.get("response_type"));
+    assertEquals("Please input less than or equal to 5 tags.", tooManyTagsResponseMap.get("error"));
+
+    // more than 5 tags
+    queryParams.put("tags", "tag1,tag2,tag3,tag4,tag5,,");
+    MockRequest tooManyTags2MockRequest = new MockRequest(queryParams, body);
+    Object tooManyTags2Result = mockAddListingHandler.handle(tooManyTags2MockRequest, mockResponse);
+    Map<String, Object> tooManyTags2ResponseMap = Utils.fromMoshiJson(
+      tooManyTags2Result.toString());
+    assertEquals("failure", tooManyTags2ResponseMap.get("response_type"));
+    assertEquals("Each tag should have a value.", tooManyTags2ResponseMap.get("error"));
+
+    // more than 2 words in tag
+    queryParams.put("tags", "more than 2 word tag");
+    MockRequest tooManyWordsInTagMockRequest = new MockRequest(queryParams, body);
+    Object tooManyWordsInTagResult = mockAddListingHandler.handle(tooManyWordsInTagMockRequest, mockResponse);
+    Map<String, Object> tooManyWordsInTagResponseMap = Utils.fromMoshiJson(tooManyWordsInTagResult.toString());
+    assertEquals("failure", tooManyWordsInTagResponseMap.get("response_type"));
+    assertEquals("Each tag should be less than or equal to 2 words.", tooManyWordsInTagResponseMap.get("error"));
+
+    // duplicate entry tag
+    queryParams.put("tags", "tag1,tag1");
+    MockRequest duplicateTagMockRequest = new MockRequest(queryParams, body);
+    Object duplicateTagResult = mockAddListingHandler.handle(duplicateTagMockRequest, mockResponse);
+    Map<String, Object> duplicateWordsTagResponseMap = Utils.fromMoshiJson(duplicateTagResult.toString());
+    assertEquals("failure", duplicateWordsTagResponseMap.get("response_type"));
+    assertEquals("Please make sure all tags are unique.", duplicateWordsTagResponseMap.get("error"));
+  }
+  }
+
+//
+//  @Test
+//  void testInvalidTags() throws ExecutionException, InterruptedException, IOException {
+//    Map<String, String> queryParams = new HashMap<>();
+//
+//    // test null tag
+//    queryParams.put("uid", "user123");
+//    queryParams.put("username", "testuser");
+//    queryParams.put("title", "Muji black pens");
+//    queryParams.put("tags", null);
+//    queryParams.put("category", "School");
+//    queryParams.put("price", "10.99");
+//    queryParams.put("condition", "New");
+//    queryParams.put("description",
+//      "I bought some Muji black pens in high school and brought them to Brown."
+//        + " Since coming here though, I only take notes using my ipad. I would hate for these pens"
+//        + " to go to worst. They are really nice. Let me know if you would like any!");
+//
+//    String workingDirectory = System.getProperty("user.dir");
+//    Path base64TestPath = Paths.get(workingDirectory, "src/test/java/edu/brown/cs/student/main/server/test/data/base64_testing_image.txt");
+//    String body = new String(Files.readAllBytes(base64TestPath));
+//    MockRequest mockRequest = new MockRequest(queryParams, body);
+//
+//    MockResponse mockResponse = new MockResponse();
+//    Object result = mockAddListingHandler.handle(mockRequest, mockResponse);
+//
+//    // Verify the result
+//    assertNotNull(result, "Result should not be null");
+//
+//    // Check for correct ResponseMap
+//    Map<String, Object> responseMap = Utils.fromMoshiJson(result.toString());
+//    assertEquals("failure", responseMap.get("response_type"));
+//  }
+//
+//  @Test
+//  void testInvalidTags() throws ExecutionException, InterruptedException, IOException {
+//    Map<String, String> queryParams = new HashMap<>();
+//
+//    // test null tag
+//    queryParams.put("uid", "user123");
+//    queryParams.put("username", "testuser");
+//    queryParams.put("title", "Muji black pens");
+//    queryParams.put("tags", null);
+//    queryParams.put("category", "School");
+//    queryParams.put("price", "10.99");
+//    queryParams.put("condition", "New");
+//    queryParams.put("description",
+//      "I bought some Muji black pens in high school and brought them to Brown."
+//        + " Since coming here though, I only take notes using my ipad. I would hate for these pens"
+//        + " to go to worst. They are really nice. Let me know if you would like any!");
+//
+//    String workingDirectory = System.getProperty("user.dir");
+//    Path base64TestPath = Paths.get(workingDirectory, "src/test/java/edu/brown/cs/student/main/server/test/data/base64_testing_image.txt");
+//    String body = new String(Files.readAllBytes(base64TestPath));
+//    MockRequest mockRequest = new MockRequest(queryParams, body);
+//
+//    MockResponse mockResponse = new MockResponse();
+//    Object result = mockAddListingHandler.handle(mockRequest, mockResponse);
+//
+//    // Verify the result
+//    assertNotNull(result, "Result should not be null");
+//
+//    // Check for correct ResponseMap
+//    Map<String, Object> responseMap = Utils.fromMoshiJson(result.toString());
+//    assertEquals("failure", responseMap.get("response_type"));
+//  }
+//
+//  @Test
+//  void testInvalidTags() throws ExecutionException, InterruptedException, IOException {
+//    Map<String, String> queryParams = new HashMap<>();
+//
+//    // test null tag
+//    queryParams.put("uid", "user123");
+//    queryParams.put("username", "testuser");
+//    queryParams.put("title", "Muji black pens");
+//    queryParams.put("tags", null);
+//    queryParams.put("category", "School");
+//    queryParams.put("price", "10.99");
+//    queryParams.put("condition", "New");
+//    queryParams.put("description",
+//      "I bought some Muji black pens in high school and brought them to Brown."
+//        + " Since coming here though, I only take notes using my ipad. I would hate for these pens"
+//        + " to go to worst. They are really nice. Let me know if you would like any!");
+//
+//    String workingDirectory = System.getProperty("user.dir");
+//    Path base64TestPath = Paths.get(workingDirectory, "src/test/java/edu/brown/cs/student/main/server/test/data/base64_testing_image.txt");
+//    String body = new String(Files.readAllBytes(base64TestPath));
+//    MockRequest mockRequest = new MockRequest(queryParams, body);
+//
+//    MockResponse mockResponse = new MockResponse();
+//    Object result = mockAddListingHandler.handle(mockRequest, mockResponse);
+//
+//    // Verify the result
+//    assertNotNull(result, "Result should not be null");
+//
+//    // Check for correct ResponseMap
+//    Map<String, Object> responseMap = Utils.fromMoshiJson(result.toString());
+//    assertEquals("failure", responseMap.get("response_type"));
+//  }
+//
+//  @Test
+//  void testInvalidTags() throws ExecutionException, InterruptedException, IOException {
+//    Map<String, String> queryParams = new HashMap<>();
+//
+//    // test null tag
+//    queryParams.put("uid", "user123");
+//    queryParams.put("username", "testuser");
+//    queryParams.put("title", "Muji black pens");
+//    queryParams.put("tags", null);
+//    queryParams.put("category", "School");
+//    queryParams.put("price", "10.99");
+//    queryParams.put("condition", "New");
+//    queryParams.put("description",
+//      "I bought some Muji black pens in high school and brought them to Brown."
+//        + " Since coming here though, I only take notes using my ipad. I would hate for these pens"
+//        + " to go to worst. They are really nice. Let me know if you would like any!");
+//
+//    String workingDirectory = System.getProperty("user.dir");
+//    Path base64TestPath = Paths.get(workingDirectory, "src/test/java/edu/brown/cs/student/main/server/test/data/base64_testing_image.txt");
+//    String body = new String(Files.readAllBytes(base64TestPath));
+//    MockRequest mockRequest = new MockRequest(queryParams, body);
+//
+//    MockResponse mockResponse = new MockResponse();
+//    Object result = mockAddListingHandler.handle(mockRequest, mockResponse);
+//
+//    // Verify the result
+//    assertNotNull(result, "Result should not be null");
+//
+//    // Check for correct ResponseMap
+//    Map<String, Object> responseMap = Utils.fromMoshiJson(result.toString());
+//    assertEquals("failure", responseMap.get("response_type"));
+//  }
+//}
